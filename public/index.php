704121ca7dc55cf390852fe60dc2663b5f7a1021
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2020-12-31 00:15:20 +0530

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home | andsol interiors</title>
+    <!-- stylesheet -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/draft.style.css">
+    <link rel="stylesheet" href="css/draft.responsive.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+</head>
+<body>
+    <div class="body__wrap">
+        <div class="body__header">
+            <header>
+                <div class="header__brand">
+                    <a href="index.php">
+                        <img src="" alt="andsol interiors logo">
+                    </a>
+                </div>
+                <!-- end header brand -->
+                <nav class="header__nav">
+                    <ul>
+                        <li class="nav__list"><a href="index.php" class="nav__link">HOME</a></li>
+                        <li class="nav__list"><a href="about.php" class="nav__link">ABOUT US</a></li>
+                        <li class="nav__list"><a href="project.php" class="nav__link">PROJECTS</a></li>
+                        <li class="nav__list"><a href="service.php" class="nav__link">SERVICE</a></li>
+                        <li class="nav__list"><a href="contact.php" class="nav__link">CONTACT US</a></li>
+                    </ul>
+                </nav>
+                <!-- end header nav  -->
+            </header>
+        </div>
+        <!-- end body header -->
+
+        <div class="body__banner"></div>
+        <!-- end body banner -->
+
+        <div class="body__container"></div>
+        <!-- end body container -->
+
+        <div class="body__footer">
+            <footer>
+            
+            </footer>
+        </div>
+        <!-- end body footer -->
+
+    </div>
+    <!-- end body wrap -->
+
+
+    <!-- script -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
