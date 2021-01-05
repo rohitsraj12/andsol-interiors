dbcb0698a7d0f1e70e8eed38dcae6824f140b629
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2021-01-05 23:42:36 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>home | andsol interiors</title>
     <!-- stylesheet -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/draft.style.css">
     <link rel="stylesheet" href="css/draft.responsive.css">
@@ -14,36 +15,240 @@
 <body>
     <div class="body__wrap">
         <div class="body__header">
-            <header>
-                <div class="header__brand">
-                    <a href="index.php">
-                        <img src="" alt="andsol interiors logo">
-                    </a>
-                </div>
-                <!-- end header brand -->
-                <nav class="header__nav">
-                    <ul>
-                        <li class="nav__list"><a href="index.php" class="nav__link">HOME</a></li>
-                        <li class="nav__list"><a href="about.php" class="nav__link">ABOUT US</a></li>
-                        <li class="nav__list"><a href="project.php" class="nav__link">PROJECTS</a></li>
-                        <li class="nav__list"><a href="service.php" class="nav__link">SERVICE</a></li>
-                        <li class="nav__list"><a href="contact.php" class="nav__link">CONTACT US</a></li>
-                    </ul>
-                </nav>
-                <!-- end header nav  -->
-            </header>
+           
+                <header>
+                    <div class="header__brand">
+                        <a href="index.php">
+                            <img src="img/brand/andsol_interiors.jpeg" height="80px" alt="andsol interiors logo">
+                        </a>
+                    </div>
+                    <!-- end header brand -->
+                    <nav class="header__nav">
+                        <ul>
+                            <li class="nav__list"><a href="index.php" class="nav__link">HOME</a></li>
+                            <li class="nav__list"><a href="about.php" class="nav__link">ABOUT US</a></li>
+                            <li class="nav__list"><a href="project.php" class="nav__link">PROJECTS</a></li>
+                            <li class="nav__list"><a href="service.php" class="nav__link">SERVICES</a></li>
+                            <li class="nav__list"><a href="contact.php" class="nav__link">CONTACT US</a></li>
+                        </ul>
+                    </nav>
+                    <!-- end header nav  -->
+                </header>
+            
         </div>
         <!-- end body header -->
 
-        <div class="body__banner"></div>
+        <div class="body__banner">
+            <div class="wrap__banner">
+                <div class="banner__mobile">
+
+                </div>
+                <!-- end banner mobile -->
+                <div class="banner__desktop">
+                    <img src="img/banner/banner.jpg" alt="andsol interiors banner">
+                </div>
+                <!-- end desktop banner -->
+            </div>
+            <!-- end wrap banner -->
+        </div>
         <!-- end body banner -->
 
-        <div class="body__container"></div>
+        <div class="body__container">
+            <main>
+                <section class="section__offer py-5">
+                    <div class="container">
+                        <div class="wrap__header py-4">
+                            <h2 class="section__header">
+                                <strong>andsol interiors</strong> offers
+                            </h2>
+                        </div>
+                        <!-- end wrap header -->
+                    </div>
+                    <div class="container">
+                        <section class="row">
+                            <article class="col-sm-3">
+                                <figure class="offer__figure">
+                                    <?xml version="1.0" encoding="iso-8859-1"?>
+                                    <!-- Generator: Adobe Illustrator 22.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+                                    <svg class="offer__icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_3" x="0px" y="0px" viewBox="0 0 64 64" style="enable-background:new 0 0 64 64;" xml:space="preserve">
+                                    <g>
+                                        <path d="M62,31h-1v-7c0-0.553-0.448-1-1-1h-1v-4h-2v4h-1c-0.552,0-1,0.447-1,1v7h-4v-3c0-0.553-0.448-1-1-1H38H26   c-0.552,0-1,0.447-1,1v3h-4v-7c0-0.553-0.448-1-1-1h-1c0-1.352-0.898-2.497-2.129-2.871C16.497,18.898,15.352,18,14,18   s-2.497,0.898-2.871,2.129C9.898,20.503,9,21.648,9,23H8c-0.552,0-1,0.447-1,1v7H2c-0.552,0-1,0.447-1,1v4c0,0.553,0.448,1,1,1h1   v25c0,0.553,0.448,1,1,1h56c0.552,0,1-0.447,1-1V37h1c0.552,0,1-0.447,1-1v-4C63,31.447,62.552,31,62,31z M57,25h2v6h-2V25z M49,31   H39v-2h10V31z M27,29h10v2H27V29z M12,22c0.552,0,1-0.447,1-1c0-0.552,0.449-1,1-1s1,0.448,1,1c0,0.553,0.448,1,1,1   c0.551,0,1,0.448,1,1h-6C11,22.448,11.449,22,12,22z M9,25h10v6H9V25z M5,37h26v24H5V37z M59,61H33V37h26V61z M61,35h-1H4H3v-2h5   h12h6h12h12h6h4h1V35z"/>
+                                        <path d="M28,47H8c-0.552,0-1,0.447-1,1v10c0,0.553,0.448,1,1,1h20c0.552,0,1-0.447,1-1V48C29,47.447,28.552,47,28,47z M27,49v4H9   v-4H27z M9,57v-2h18v2H9z"/>
+                                        <path d="M10,45c1.654,0,3-1.346,3-3s-1.346-3-3-3s-3,1.346-3,3S8.346,45,10,45z M10,41c0.551,0,1,0.448,1,1s-0.449,1-1,1   s-1-0.448-1-1S9.449,41,10,41z"/>
+                                        <path d="M18,45c1.654,0,3-1.346,3-3s-1.346-3-3-3s-3,1.346-3,3S16.346,45,18,45z M18,41c0.551,0,1,0.448,1,1s-0.449,1-1,1   s-1-0.448-1-1S17.449,41,18,41z"/>
+                                        <path d="M26,45c1.654,0,3-1.346,3-3s-1.346-3-3-3s-3,1.346-3,3S24.346,45,26,45z M26,41c0.551,0,1,0.448,1,1s-0.449,1-1,1   s-1-0.448-1-1S25.449,41,26,41z"/>
+                                        <path d="M36,59h20c0.552,0,1-0.447,1-1V48c0-0.553-0.448-1-1-1H36c-0.552,0-1,0.447-1,1v10C35,58.553,35.448,59,36,59z M37,57v-2   h18v2H37z M55,49v4H37v-4H55z"/>
+                                        <path d="M38,45c1.654,0,3-1.346,3-3s-1.346-3-3-3s-3,1.346-3,3S36.346,45,38,45z M38,41c0.551,0,1,0.448,1,1s-0.449,1-1,1   s-1-0.448-1-1S37.449,41,38,41z"/>
+                                        <path d="M46,45c1.654,0,3-1.346,3-3s-1.346-3-3-3s-3,1.346-3,3S44.346,45,46,45z M46,41c0.551,0,1,0.448,1,1s-0.449,1-1,1   s-1-0.448-1-1S45.449,41,46,41z"/>
+                                        <path d="M54,45c1.654,0,3-1.346,3-3s-1.346-3-3-3s-3,1.346-3,3S52.346,45,54,45z M54,41c0.551,0,1,0.448,1,1s-0.449,1-1,1   s-1-0.448-1-1S53.449,41,54,41z"/>
+                                        <path d="M8,13h48c0.369,0,0.708-0.203,0.882-0.528c0.174-0.325,0.155-0.72-0.05-1.026l-4-6C52.646,5.167,52.334,5,52,5H36V1h-2v4   h-4V1h-2v4H12c-0.334,0-0.646,0.167-0.832,0.445l-4,6c-0.205,0.307-0.224,0.701-0.05,1.026C7.292,12.797,7.631,13,8,13z M12.535,7   H29h6h16.465l2.667,4H9.869L12.535,7z"/>
+                                    </g>
+                                    </svg>
+
+                                </figure>
+                                <header class="offer__header">
+                                    MODULAR
+                                </header>
+                                <div class="offer__body">
+                                    <ul>
+                                        <li>kitchen</li>
+                                        <li>wardrobe</li>
+                                        <li>storage</li>
+                                    </ul>
+                                </div>
+                            </article>
+                            <article class="col-sm-3">
+                                <figure></figure>
+                                <header class="offer__header"></header>
+                                <div class="offer__body"></div>
+                            </article>
+                            <article class="col-sm-3">
+                                <figure></figure>
+                                <header class="offer__header"></header>
+                                <div class="offer__body"></div>
+                            </article>
+                            <article class="col-sm-3">
+                                <figure></figure>
+                                <header class="offer__header"></header>
+                                <div class="offer__body"></div>
+                            </article>
+                        </section>
+                    </div>
+                </section>
+                <!-- end section offer -->
+            </main>
+           
+        </div>
         <!-- end body container -->
 
         <div class="body__footer">
             <footer>
-            
+                <div class="container">
+                    <section class="section__footer">
+                        <div class="row">
+                            <article class="col-sm-3">
+                                <header class="footer__article-header">
+                                    <a href="index.php">
+                                        <img src="img/brand/andsol_interiors.jpeg" height="80px" alt="andsol interiors logo">
+                                    </a>
+                                </header>
+                                <!-- end footer article header -->
+                                <div class="footer__article-body">
+                                   <div class="footer__article-body-top">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, magnam!</p>
+                                   </div>
+                                   <!-- end footer artcle body top -->
+                                    <div class="footer__article-body-bottom">
+                                        <ul class="footer__social">
+                                            <li class="footer__social-list">
+                                                <a href="#" class="footer__social-link">
+                                                    <i class="fa fa-facebook" aria-hidden="true"></i>
+                                                </a>
+                                            </li>
+                                            <li class="footer__social-list">
+                                            <a href="#" class="footer__social-link">
+                                                <i class="fa fa-whatsapp" aria-hidden="true"></i>
+                                            </a>
+                                            </li>
+                                            <li class="footer__social-list">
+                                                <a href="#" class="footer__social-link">
+                                                    <i class="fa fa-twitter" aria-hidden="true"></i>
+                                                </a>
+                                            </li>
+                                            <li class="footer__social-list">
+                                                <a href="#" class="footer__social-link">
+                                                    <i class="fa fa-linkedin" aria-hidden="true"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <!-- end footer article body bottom -->
+                                </div>
+
+                                <!-- end footer article body -->
+                            </article>
+                            <article class="col-sm-3">
+                                <header class="footer__article-header">
+                                    QUICK LINKS
+                                </header>
+                                <div>
+                                    <ul class="footer__quick-links">
+                                        <li class="footer__quick-links-list">
+                                            <a href="index.php">Home</a>
+                                        </li>
+                                        <li class="footer__quick-links-list">
+                                            <a href="about.php">About us</a>
+                                        </li>
+                                        <li class="footer__quick-links-list">
+                                            <a href="service.php">Services</a>
+                                        </li>
+                                        <li class="footer__quick-links-list">
+                                            <a href="contact.php">Contact us</a>
+                                        </li>
+                                        <li class="footer__quick-links-list">
+                                            <a href="contact.php">FAQs</a>
+                                        </li>
+                                        <li class="footer__quick-links-list">
+                                            <a href="contact.php">Site map</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </article>
+                            <article class="col-sm-3">
+                                <header class="footer__article-header">
+                                    PROJECTS
+                                </header>
+                                <div>
+                                    <ul>
+                                        <li>1</li>
+                                        <li>2</li>
+                                        <li>3</li>
+                                        <li>4</li>
+                                        <li>5</li>
+                                    </ul>
+                                </div>
+                            </article>
+                            <article class="col-sm-3">
+                                <header class="footer__article-header">
+                                    Get in touch  
+                                </header>
+                                <div class="footer__article-body">
+                                    <ul class="footer__contact">
+                                        <li class="footer__contact-list">
+                                            <div class="left__block">
+                                                <i class="fa fa-phone" aria-hidden="true"></i>
+                                            </div>
+                                            <div class="right__block">
+                                                <a class="right__block-phone" href="tel:9945869856" target="_blank">+91-9945869856</a>
+                                                <a class="right__block-phone" href="tel:9538832255" target="_blank">+91-9538832255</a>
+                                            </div>
+                                        </li>
+                                        <li class="footer__contact-list">
+                                            <div class="left__block">
+                                                <i class="fa fa-envelope" aria-hidden="true"></i>
+                                            </div>
+                                            <div class="right__block">
+                                                <a href="mailto:contact@andsolinteriors.com">contact@andsolinteriors.com</a>
+                                                <a href="mailto:andsolv2v@gmail.com">andsolv2v@gmail.com</a>
+                                            </div>
+                                        </li>
+                                        <li class="footer__contact-list">
+                                            <div class="left__block">
+                                                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                            </div>
+                                            <div class="right__block">
+                                                <address>
+                                                    6th cross, Yellenahalli main road, Akshayanagar,</br> near Karnataka bank, DLF Town, Bengaluru,</br> Karnataka 560114.
+                                                </address>
+                                            </div>
+                                        </li>
+                                       
+                                    </ul>
+                                </div>
+                            </article>
+                        </div>
+                    </section>
+                </div>
             </footer>
         </div>
         <!-- end body footer -->
