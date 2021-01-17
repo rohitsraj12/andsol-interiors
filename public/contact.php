e5fe630ed2e6f739959decd27d754f7675bf8094
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2021-01-17 15:10:55 +0530

    Update contact.php

--- public/contact.php
+++ public/contact.php
@@ -67,30 +67,93 @@
                             </h2>
                         </div>
                         <!-- end wrap header -->
-                        <div class="row">
-                            <article class="col-sm-6">
-                                <div class="article__header">
-                                    <h4>send message us</h4>
-                                </div>
-                                <div class="article__body">
-                                <form class="row g-3">
-                                    <div class="col-md-6">
-                                        <input type="text" class="form-control" id="inputEmail4" placeholder="Name">
+                        <div class="wrap__contact-section">
+                            <div class="row px-3">
+                                <article class="col-sm-4 py-4 bg-dark text-light">
+                                    <!-- <div class="article__header pb-4">
+                                        <h4 class="section__sub-header">Contact details</h4>
+                                    </div> -->
+                                    <div class="article__body">
+                                        <ul class="contact__details">
+                                            <li class="pb-3">
+                                                <div class="contact__details-icon">
+                                                    <i class="fa fa-map-marker mr-3" aria-hidden="true"></i> 
+                                                </div>
+                                                <div class="pl-3">
+                                                    <span class="mb-2 d-block span__header">Our office address:</span>
+                                                    <address class="">
+                                                        6th cross, Yellenahalli main road, Akshayanagar,</br> near Karnataka bank, DLF Town, Bengaluru,</br> Karnataka 560114.
+                                                    </address>
+                                                </div>
+                                            
+                                            </li>
+                                            <li class="pb-3">
+                                                <div class="contact__details-icon">
+                                                    <i class="fa fa-envelope-o mr-3" aria-hidden="true"></i>
+                                                </div>
+                                                <div>
+                                                    <span class="mb-2 d-block span__header">General enquiries:</span>
+                                                    <a class="contact__details-link text-light" href="mailto:contact@andsolinteriors.com" target="_blank">contact@andsolinteriors.com</a>
+                                                    <a class="contact__details-link text-light" href="mailto:andsolv2v@gmail.com" target="_blank">andsolv2v@gmail.com</a>
+                                                </div>
+                                                
+                                            </li>
+                                            <li class="pb-3">
+                                                <div class="contact__details-icon">
+                                                    <i class="fa fa-phone mr-3" aria-hidden="true"></i>
+                                                
+                                                </div>
+                                                <div class="pl-1">
+                                                    <span class="mb-2 d-block span__header">Call us:</span>
+                                                    <a class="contact__details-link text-light" class="" href="tel:9945869856" target="_blank">+91-9945869856</a>
+                                                    <a class="contact__details-link text-light" class="" href="tel:9538832255" target="_blank">+91-9538832255</a>
+                                                </div>
+                                                
+                                            </li>
+                                            <li class="pb-3">
+                                                <div class="contact__details-icon">
+                                                    <i class="fa fa-clock-o mr-3" aria-hidden="true"></i>
+                                                </div>
+                                                    <div class="pl-1">
+                                                    <span class="mb-2 d-block span__header">Our timing:</span>
+                                                    <span > Mon - Sun : 09:30 AM - 06:00 PM</span>
+                                                    </div>
+                                                
+                                            </li>
+                                        </ul>
                                     </div>
-                                    <div class="col-md-6">
-                                        <input type="email" class="form-control" id="inputPassword4" placeholder="Email">
+                                </article>
+                                <article class="col-sm-8 py-4">
+                                    <!-- <div class="article__header">
+
+                                        <h4 class="section__sub-header">send message us</h4>
+                                    </div> -->
+                                    <div class="article__body">
+                                    <form class="row g-3" method="POST" action="">
+                                        <div class="col-12 pt-3">
+                                            <label for="inputName" class="form-label">Name</label>
+                                            <i class="fa fa-user-o contact__form-icon" aria-hidden="true"></i>
+                                            <input type="text" class="form-control" id="inputName" placeholder="Name" name="name">
+                                        </div>
+                                        <div class="col-12 pt-3">
+                                            <label for="inputEmail" class="form-label">Email</label>
+                                            <i class="fa fa-envelope-o contact__form-icon" aria-hidden="true"></i>
+                                            <input type="email" class="form-control" id="inputEmail" placeholder="Email" name="email">
+                                        </div>
+                                        <div class="col-12 pt-3">
+                                            <label for="inputPhone" class="form-label">Phone number</label>
+                                            <i class="fa fa-phone contact__form-icon" aria-hidden="true"></i>
+                                            <input type="text" class="form-control" id="inputPhone" placeholder="Phone Number" name="phone">
+                                        </div>
+                                        <div class="col-12 pt-4">
+                                            <input type="submit" class="btn btn__primary" value="SEND MESSAGE">
+                                        </div>
+                                    </form>
                                     </div>
-                                    <div class="col-12">
-                                        <input type="text" class="form-control" id="inputAddress" placeholder="Phone Number">
-                                    </div>
-                                    <div class="col-12">
-                                        <button type="submit" class="btn btn-primary">SEND MESSAGE</button>
-                                    </div>
-                                </form>
-                                </div>
-                            </article>
-                            <article class="col-sm-6"></article>
+                                </article>
+                            </div>
                         </div>
+                        
                     </div>
                 </section>
             </main>   
