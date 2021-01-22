ce532977563bf1db1dc060787b55d5e43dc4298d
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2021-01-22 20:36:44 +0530

    Create header.php

---
+++ private/required/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home | andsol interiors</title>
+    <!-- stylesheet -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/draft.style.css">
+    <link rel="stylesheet" href="css/draft.responsive.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+</head>
+<body>
+    <div class="body__wrap">
+        <div class="body__header">
+           
+                <header>
+                    <div class="header__brand">
+                        <a href="index.php">
+                            <img src="img/brand/andsol_interiors.jpeg" height="80px" alt="andsol interiors logo">
+                        </a>
+                    </div>
+                    <!-- end header brand --> 
+                    <div class="hamburger">
+                            <a href=""><i class="fa fa-bars" aria-hidden="true"></i></a>
+                    </div>
+                    <nav class="header__nav">
+                       
+                        <ul>
+                            <li class="nav__list"><a href="index.php" class="nav__link">HOME</a></li>
+                            <li class="nav__list"><a href="about.php" class="nav__link">ABOUT US</a></li>
+                            <li class="nav__list"><a href="project.php" class="nav__link">PROJECTS</a></li>
+                            <li class="nav__list"><a href="service.php" class="nav__link">SERVICES</a></li>
+                            <li class="nav__list"><a href="contact.php" class="nav__link">CONTACT US</a></li>
+                        </ul>
+                    </nav>
+                    <!-- end header nav  -->
+                </header>
+            
+        </div>
+        <!-- end body header -->
+
