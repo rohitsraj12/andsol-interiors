7ad3d7b1b48f57acc33c32a039de2e6c62499579
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2021-01-22 20:36:41 +0530

    Create footer.php

---
+++ private/required/footer.php
@@ -0,0 +1,143 @@
+<div class="body__footer">
+            <footer>
+                <div class="container">
+                    <section class="section__footer">
+                        <div class="row">
+                            <article class="col-sm-4">
+                                <header class="footer__article-header">
+                                    <a href="index.php">
+                                        <img src="img/brand/andsol_interiors.jpeg" height="80px" alt="andsol interiors logo">
+                                    </a>
+                                </header>
+                                <!-- end footer article header -->
+                                <div class="footer__article-body">
+                                   <div class="footer__article-body-top">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, magnam!</p>
+                                   </div>
+                                   <!-- end footer artcle body top -->
+                                    <div class="footer__article-body-bottom">
+                                        <ul class="footer__social">
+                                            <li class="footer__social-list">
+                                                <a href="#" class="footer__social-link">
+                                                    <i class="fa fa-facebook" aria-hidden="true"></i>
+                                                </a>
+                                            </li>
+                                            <li class="footer__social-list">
+                                            <a href="#" class="footer__social-link">
+                                                <i class="fa fa-whatsapp" aria-hidden="true"></i>
+                                            </a>
+                                            </li>
+                                            <li class="footer__social-list">
+                                                <a href="#" class="footer__social-link">
+                                                    <i class="fa fa-twitter" aria-hidden="true"></i>
+                                                </a>
+                                            </li>
+                                            <li class="footer__social-list">
+                                                <a href="#" class="footer__social-link">
+                                                    <i class="fa fa-linkedin" aria-hidden="true"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <!-- end footer article body bottom -->
+                                </div>
+
+                                <!-- end footer article body -->
+                            </article>
+                            <article class="col-sm-2">
+                                <header class="footer__article-header">
+                                    QUICK LINKS
+                                </header>
+                                <div>
+                                    <ul class="footer__quick-links">
+                                        <li class="footer__quick-links-list">
+                                            <a href="index.php">Home</a>
+                                        </li>
+                                        <li class="footer__quick-links-list">
+                                            <a href="about.php">About us</a>
+                                        </li>
+                                        <li class="footer__quick-links-list">
+                                            <a href="service.php">Services</a>
+                                        </li>
+                                        <li class="footer__quick-links-list">
+                                            <a href="contact.php">Contact us</a>
+                                        </li>
+                                        <li class="footer__quick-links-list">
+                                            <a href="faq.php">FAQs</a>
+                                        </li>
+                                        <li class="footer__quick-links-list">
+                                            <a href="contact.php">Site map</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </article>
+                            <article class="col-sm-2">
+                                <header class="footer__article-header">
+                                    PROJECTS
+                                </header>
+                                <div>
+                                    <ul>
+                                        <li>1</li>
+                                        <li>2</li>
+                                        <li>3</li>
+                                        <li>4</li>
+                                        <li>5</li>
+                                    </ul>
+                                </div>
+                            </article>
+                            <article class="col-sm-4">
+                                <header class="footer__article-header">
+                                    GET IN TOUCH 
+                                </header>
+                                <div class="footer__article-body">
+                                    <ul class="footer__contact">
+                                        <li class="footer__contact-list">
+                                            <div class="left__block">
+                                                <i class="fa fa-phone" aria-hidden="true"></i>
+                                            </div>
+                                            <div class="right__block">
+                                                <a class="right__block-phone" href="tel:9945869856" target="_blank">+91-9945869856</a>
+                                                <a class="right__block-phone" href="tel:9538832255" target="_blank">+91-9538832255</a>
+                                            </div>
+                                        </li>
+                                        <li class="footer__contact-list">
+                                            <div class="left__block">
+                                                <i class="fa fa-envelope" aria-hidden="true"></i>
+                                            </div>
+                                            <div class="right__block">
+                                                <a href="mailto:contact@andsolinteriors.com">contact@andsolinteriors.com</a>
+                                                <a href="mailto:andsolv2v@gmail.com">andsolv2v@gmail.com</a>
+                                            </div>
+                                        </li>
+                                        <li class="footer__contact-list">
+                                            <div class="left__block">
+                                                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                            </div>
+                                            <div class="right__block">
+                                                <address>
+                                                    6th cross, Yellenahalli main road, Akshayanagar,</br> near Karnataka bank, DLF Town, Bengaluru,</br> Karnataka 560114.
+                                                </address>
+                                            </div>
+                                        </li>
+                                       
+                                    </ul>
+                                </div>
+                            </article>
+                        </div>
+                    </section>
+                </div>
+            </footer>
+        </div>
+        <!-- end body footer -->
+
+    </div>
+    <!-- end body wrap -->
+
+
+    <!-- script -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
