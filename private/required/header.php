e45f9efc56684b2e5689b9728d2a8bf550e0c5f2
rohitsraj12 <rohitsraj12@gmail.com>
2021-01-24 11:08:03 +0530

    Update header.php

--- private/required/header.php
+++ private/required/header.php
@@ -7,11 +7,10 @@
     <!-- stylesheet -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/draft.style.css">
     <link rel="stylesheet" href="css/draft.responsive.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/responsive.css">
-</head>
+    <link rel="stylesheet" href="css/draft.style.css">
 <body>
     <div class="body__wrap">
         <div class="body__header">
