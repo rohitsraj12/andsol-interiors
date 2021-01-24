2bec15aa30d280c1b0284663dc8e90eec8b44083
rohitsraj12 <rohitsraj12@gmail.com>
2021-01-24 13:56:16 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
         $(".header__nav").slideToggle();
     });
     $(".faq__header").click(function(){
-        $(this).find(i).toggleClass(".fa-sort-desc");
-        $(this).find(i).toggleClass(".fa-sort-asc");
+        // $(this).find(i).toggleClass(".fa-sort-desc");
+        // $(this).find(i).toggleClass(".fa-sort-asc");
         $(this).parent().find("footer").slideToggle(1000);
     });
     // alert("hi");
