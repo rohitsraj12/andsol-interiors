7c0b3cf3104c03fcd4bab956ae48a183aac65743
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2021-01-24 11:07:44 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
     $(".hamburger").click(function(){
-        $(".header__nav").slideDown();
+        $(".header__nav").slideToggle();
     });
     $(".faq__header").click(function(){
         $(this).find(i).toggleClass(".fa-sort-desc");
         $(this).find(i).toggleClass(".fa-sort-asc");
         $(this).parent().find("footer").slideToggle(1000);
     });
+    // alert("hi");
 });
