87dd56bb2940e57c131b92f719e7424aeb82912b
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2021-02-01 00:33:15 +0530

    Create gallery.php

---
+++ public/gallery.php
@@ -0,0 +1,45 @@
+
+        <?php
+            include("../private/required/header.php");
+        
+        ?>
+
+        <div class="body__banner half__banner">
+            <div class="wrap__banner">
+                <div class="banner__mobile">
+
+                </div>
+                <!-- end banner mobile -->
+                <div class="banner__desktop">
+                    <img src="img/banner/banner.jpg" alt="andsol interiors banner">
+                </div>
+                <!-- end desktop banner -->
+            </div>
+            <!-- end wrap banner -->
+        </div>
+        <!-- end body banner -->
+
+        <div class="body__container">
+            <main>
+                <section class="section__offer py-5">
+                    <div class="container">
+                        <div class="wrap__header py-4">
+                            <h2 class="section__header">
+                                Gallery
+                            </h2>
+                        </div>
+                        <!-- end wrap header -->
+                    </div>
+                    <div class="container">
+
+                    </div>
+                </section>
+            </main>   
+        </div>
+        <!-- end body container -->
+   
+        
+        <?php
+            include("../private/required/footer.php");
+        
+        ?>
