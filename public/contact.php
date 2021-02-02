b70c252472801c423ac679c0e4ca412a2b8c6dd2
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2021-02-02 23:17:22 +0530

    Update contact.php

--- public/contact.php
+++ public/contact.php
@@ -11,7 +11,9 @@
                 </div>
                 <!-- end banner mobile -->
                 <div class="banner__desktop">
-                    <img src="img/banner/banner.jpg" alt="andsol interiors banner">
+                  
+                    <!-- <img src="img/banner/banner.jpg" alt="andsol interiors banner"> -->
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.584359406854!2d77.61418891482093!3d12.870100690922518!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae6b2ef7fde993%3A0x84380947a92b4f12!2sYelenahalli%20Main%20Rd%20%26%206th%20Cross%20Rd%2C%20Akshayanagara%20East%2C%20Akshayanagar%2C%20Bengaluru%2C%20Karnataka%20560068!5e0!3m2!1sen!2sin!4v1612287093030!5m2!1sen!2sin" class="w-100" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 </div>
                 <!-- end desktop banner -->
             </div>
