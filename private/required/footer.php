6510aac79d1df2f6030c8c4b64ef92588468740b
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2021-02-04 16:25:37 +0530

    Update footer.php

--- private/required/footer.php
+++ private/required/footer.php
@@ -144,5 +144,18 @@
     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="js/script.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $(".fancybox").attr("data-fancybox","mygallery");
+            // assign captions and title from alt-attributes of images:
+            $(".fancybox").each(function(){
+                $(this).attr("data-caption", $(this).find("img").attr("alt"));
+                $(this).attr("title", $(this).find("img").attr("alt"));
+            });
+            // start fancybox:
+            $(".fancybox").fancybox();
+        })
+    </script>
 </body>
 </html>
