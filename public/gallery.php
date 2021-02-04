30dc0d9a76ebfe030aa35f626ad916430053aabf
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2021-02-04 16:25:19 +0530

    Update gallery.php

--- public/gallery.php
+++ public/gallery.php
@@ -31,7 +31,150 @@
                         <!-- end wrap header -->
                     </div>
                     <div class="container">
+                        
+                        <div class="community__type">
+                                          
+                            <div class="gallery__grid gallery" >
+                                <a class="box a fancybox" href="img/project/service_01.jpg">
+                                    <img src="img/project/service_01.jpg" alt="" />
+                                    <header>
+                                        <p>
+                                            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing.
+                                        </p>
+                                        
+                                        <h2 class="gallery__header">
+                                            bannerghatta road
+                                        </h2>
+                                    </header>
+                                </a>
+                                    <a class="box b fancybox" href="img/project/service_02.jpg">
 
+                                        <img src="img/project/service_02.jpg" alt="" />
+                                    
+                                        <header>
+                                            <p>
+                                                Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet cng.
+                                            </p>
+                                            
+                                            <h2 class="gallery__header">
+                                                bannerghatta road
+                                            </h2>
+                                        </header>
+                                    </a>
+                                <a class="box c fancybox" href="img/project/service_03.jpg">
+                                    <img src="img/project/service_03.jpg" alt="" />
+                                    
+                                    <header>
+                                        
+                                        <p>
+                                            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet cng.
+                                        </p>
+                                        
+                                        <h2 class="gallery__header">
+                                            bannerghatta road
+                                        </h2>
+                                    </header>
+                                </a>
+                                <a class="box d fancybox" href="img/project/service_04.jpg">
+                                    <img src="img/project/service_04.jpg" alt="" />
+                                    
+                                    <header>
+                                        
+                                        <p>
+                                            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet cng.
+                                        </p>
+                                        
+                                        <h2 class="gallery__header">
+                                            bannerghatta road
+                                        </h2>
+                                    </header>
+                                </a>
+                                <a class="box e fancybox"  href="img/project/service_05.jpg">
+                                    <img src="img/project/service_05.jpg" alt="" />
+                                    
+                                    <header>
+                                        
+                                        <p>
+                                            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet cng.
+                                        </p>
+                                        
+                                        <h2 class="gallery__header">
+                                            bannerghatta road
+                                        </h2>
+                                    </header>
+                                </a>
+                                <a class="box f fancybox" href="img/project/service_01.jpg">
+                                    <img src="img/project/service_01.jpg" alt="" />
+                                    
+                                    <header>
+                                        
+                                        <p>
+                                            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet cng.
+                                        </p>
+                                        
+                                        <h2 class="gallery__header">
+                                            bannerghatta road
+                                        </h2>
+                                    </header>
+                                </a>
+                                <a class="box g fancybox" href="img/project/service_02.jpg">
+                                    <img src="img/project/service_02.jpg" alt="" />
+                                    
+                                    <header>
+                                        
+                                        <p>
+                                            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet cng.
+                                        </p>
+                                        
+                                        <h2 class="gallery__header">
+                                            bannerghatta road
+                                        </h2>
+                                    </header>
+                                </a>
+                                <a class="box h fancybox" href="img/project/service_03.jpg">
+                                    <img src="img/project/service_03.jpg" alt="" />
+                                    
+                                    <header>
+                                        
+                                        <p>
+                                            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet cng.
+                                        </p>
+                                        
+                                        <h2 class="gallery__header">
+                                            bannerghatta road
+                                        </h2>
+                                    </header>
+                                </a>
+                                <div class="box i fancybox" href="img/project/service_04.jpg">
+                                    <img src="img/project/service_04.jpg" alt="" />
+                                    
+                                    <header>
+                                        
+                                        <p>
+                                            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet cng.
+                                        </p>
+                                        
+                                        <h2 class="gallery__header">
+                                            bannerghatta road
+                                        </h2>
+                                    </header>
+                                </div>
+                                <div class="box j  fancybox" href="img/project/service_04.jpg">
+                                    <img src="img/project/service_04.jpg" alt="" />
+                                    
+                                    <header>
+                                        
+                                        <p>
+                                            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet cng.
+                                        </p>
+                                        
+                                        <h2 class="gallery__header">
+                                            bannerghatta road
+                                        </h2>
+                                    </header>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </section>
             </main>   
