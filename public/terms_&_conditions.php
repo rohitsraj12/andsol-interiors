e6ec0392372bcaa8fae99f20742f5b77129f0e6a
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2021-02-05 19:35:37 +0530

    Create terms_&_conditions.php

---
+++ public/terms_&_conditions.php
@@ -0,0 +1,55 @@
+<?php
+    include("../private/required/header.php");
+?>
+<div class="body__container pt-5">
+    <main>
+        <section class="section__terms-conditions py-5">
+            <div class="container">
+                <div class="wrap__header py-4">
+                    <h2 class="section__header">
+                        Terms and conditions
+                    </h2>
+                </div>
+                <div class="terms__conditions-body p-5">
+                    <ul>
+                    <li>
+                        <p>1.	The products used by Andsol are properly maintained and used for domestic use.</p>
+                    </li>
+                    <li>
+                        <p>2.	Andsol is not responsible for any kind of damage caused to products while installation.</p>
+                    </li>
+                    <li>
+                        <p>3.	Andsol reserves the right to alter specifications in keeping with its policy of continuous product evaluation and improvement.</p>
+                    </li>
+                    <li>
+                        <p>4.	Repair and/or replacement are subject to the current product offerings of styles and construction options at the time of the claim.</p>
+                    </li>
+                    <li>
+                        <p>5.	Visual or malfunctioning damage should be informed within 10 days to the Andsol team via phone or email.</p>
+                    </li>
+                    <li>
+                        <p>6.	The warranty is not renewed, in case the Andsol modular cabinets or components are replaced in part or whole during the said warranty period. </p>
+                    </li>
+                    <li>
+                        <p>7.	Warranty period- 5 years (for storage & modular wardrobes) and 10 years (for kitchens) from the date of possession.</p>
+                    </li>
+                    <li>
+                        <p>8.	The warranty policy shall not cover any defects arising out of factors out of control like natural calamities, customer negligence, etc.</p>
+                    </li>
+                    <li>
+                        <p>9.	Any delay in payments will cause delay in work and the Andsol team will not be responsible for that.</p>
+                    </li>
+                    <li>
+                        <p>10.	The terms and conditions might change or get upgraded as per the company policies. </p>
+                    </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </main>
+</div>
+
+
+<?php
+    include("../private/required/footer.php");
+?>
