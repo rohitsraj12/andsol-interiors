e3a5455f3a5e907771477f34603287fdea86aada
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2021-02-13 20:20:17 +0530

    Update contact.php

--- public/contact.php
+++ public/contact.php
@@ -16,8 +16,8 @@
                 <div class="banner__desktop">
                   
                     <!-- <img src="img/banner/banner.jpg" alt="andsol interiors banner"> -->
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.584359406854!2d77.61418891482093!3d12.870100690922518!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae6b2ef7fde993%3A0x84380947a92b4f12!2sYelenahalli%20Main%20Rd%20%26%206th%20Cross%20Rd%2C%20Akshayanagara%20East%2C%20Akshayanagar%2C%20Bengaluru%2C%20Karnataka%20560068!5e0!3m2!1sen!2sin!4v1612287093030!5m2!1sen!2sin" class="w-100" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-                </div>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.574438519333!2d77.51275341482096!3d12.87074029092206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae4155ab456ddb%3A0x8595cd3c481724e0!2sAndsol%20Interiors!5e0!3m2!1sen!2sin!4v1613208840404!5m2!1sen!2sin" class="w-100" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    </div>
                 <!-- end desktop banner -->
             </div>
             <!-- end wrap banner -->
@@ -49,7 +49,7 @@
                                                 <div class="pl-3">
                                                     <span class="mb-2 d-block span__header">Our office address:</span>
                                                     <address class="">
-                                                        6th cross, Yellenahalli main road, Akshayanagar,</br> near Karnataka bank, DLF Town, Bengaluru,</br> Karnataka 560114.
+                                                        #2221, 8th cross, Park street, BSK 6th Stage, 4th Block, Bangalore-506109   
                                                     </address>
                                                 </div>
                                             
@@ -62,6 +62,7 @@
                                                     <span class="mb-2 d-block span__header">General enquiries:</span>
                                                     <a class="contact__details-link " href="mailto:contact@andsolinteriors.com" target="_blank">contact@andsolinteriors.com</a>
                                                     <a class="contact__details-link " href="mailto:andsolv2v@gmail.com" target="_blank">andsolv2v@gmail.com</a>
+                                                    <a class="contact__details-link " href="mailto:engrs.srbuildcon@gmail.com" target="_blank">engrs.srbuildcon@gmail.com</a>
                                                 </div>
                                                 
                                             </li>
@@ -72,8 +73,8 @@
                                                 </div>
                                                 <div class="">
                                                     <span class="mb-2 d-block span__header">Call us:</span>
-                                                    <a class="contact__details-link" class="" href="tel:9945869856" target="_blank">+91-9945869856</a>
-                                                    <a class="contact__details-link" class="" href="tel:9538832255" target="_blank">+91-9538832255</a>
+                                                    <a class="contact__details-link" href="tel:9964619124" target="_blank">+91-9964619124</a>
+                                                    <a class="contact__details-link" href="tel:8197837643" target="_blank">+91-8197837643</a>
                                                 </div>
                                                 
                                             </li>
