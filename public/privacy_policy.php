e45889bed9c4e2354a369161a517f164aa2bdec6
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2021-02-18 11:40:00 +0530

    Create privacy_policy.php

---
+++ public/privacy_policy.php
@@ -0,0 +1,58 @@
+<?php
+   $page_title = "Terms and Conditions";
+   $page_meta_key = "best interior design, best home interior design, best house interior design, modular kitchen designs, home decor design, small modular kitchen, best modular kitchen, latest modular kitchen designs, budget modular kitchen, best home designers, best interior for living room, best interior design for 3bhk flat, best indian interior designer, modern interior designers, living room best design, interior bedroom wall design, best australian interior designers, best budget interior design, ideal interior design for small houses, fallcelling design, dining hall designs, arch design for dining hall, hall partition designs between living dining";
+   $page_meta_description = "";
+    include("../private/required/header.php");
+?>
+<div class="body__container pt-5">
+    <main>
+        <section class="section__terms-conditions py-5">
+            <div class="container">
+                <div class="wrap__header py-4">
+                    <h2 class="section__header">
+                        Terms and conditions
+                    </h2>
+                </div>
+                <div class="terms__conditions-body p-5">
+                    <ul>
+                    <li>
+                        <p>1.	The products used by Andsol are properly maintained and used for domestic use.</p>
+                    </li>
+                    <li>
+                        <p>2.	Andsol is not responsible for any kind of damage caused to products while installation.</p>
+                    </li>
+                    <li>
+                        <p>3.	Andsol reserves the right to alter specifications in keeping with its policy of continuous product evaluation and improvement.</p>
+                    </li>
+                    <li>
+                        <p>4.	Repair and/or replacement are subject to the current product offerings of styles and construction options at the time of the claim.</p>
+                    </li>
+                    <li>
+                        <p>5.	Visual or malfunctioning damage should be informed within 10 days to the Andsol team via phone or email.</p>
+                    </li>
+                    <li>
+                        <p>6.	The warranty is not renewed, in case the Andsol modular cabinets or components are replaced in part or whole during the said warranty period. </p>
+                    </li>
+                    <li>
+                        <p>7.	Warranty period- 5 years (for storage & modular wardrobes) and 10 years (for kitchens) from the date of possession.</p>
+                    </li>
+                    <li>
+                        <p>8.	The warranty policy shall not cover any defects arising out of factors out of control like natural calamities, customer negligence, etc.</p>
+                    </li>
+                    <li>
+                        <p>9.	Any delay in payments will cause delay in work and the Andsol team will not be responsible for that.</p>
+                    </li>
+                    <li>
+                        <p>10.	The terms and conditions might change or get upgraded as per the company policies. </p>
+                    </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </main>
+</div>
+
+
+<?php
+    include("../private/required/footer.php");
+?>
