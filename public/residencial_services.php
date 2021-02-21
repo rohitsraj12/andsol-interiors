d02f21551d030f2f9673ab2fadd05f9cc9dfdc53
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2021-02-21 13:05:22 +0530

    Create residencial_services.php

---
+++ public/residencial_services.php
@@ -0,0 +1,206 @@
+
+       
+<?php
+    $page_title = "residencial services";
+    $page_meta_key = "";
+    $page_meta_description = "";
+    include("../private/required/header.php");
+?>
+
+    <div class="body__banner half__banner">
+        <div class="wrap__banner">
+            <div class="banner__mobile">
+                <img src="img/banner/other-banner-sm.jpg" alt="andsol interiors banner">
+            </div>
+            <!-- end banner mobile -->
+            <div class="banner__desktop">
+                <img src="img/banner/other-banner.jpg" alt="andsol interiors banner">
+            </div>
+            <!-- end desktop banner -->
+        </div>
+        <!-- end wrap banner -->
+    </div>
+    <!-- end body banner -->
+    <div class="body__container">
+            <main>
+                <section class="section__services py-5">
+                    <div class="container">
+                        <div class="wrap__header py-4">
+                            <h1 class="section__header">
+                                residencial interior services 
+                            </h1>
+                        </div>
+                        <!-- end wrap header -->
+                        <div class="service__slider-wrap row pb-5">
+                            <div class="col-sm-6">
+                                    <figure >
+                                        <img src="img/services/residencial_interior_service_home.jpeg" alt="andsol residencial interior services">
+                                    </figure>
+                            </div>
+                            <div class="col-sm-6">
+                                <article class="home__service-block">
+                                    <header>
+                                        <h2 class="service__header_1 section__sub-header">
+                                        3D Interior Designing
+                                        </h2>
+                                    </header>
+                                    <div class="home__service-body">
+                                        <p>High-quality 3D interior designing services keeping the demands of our clients in mind.</p>
+                                    </div>
+                                    <!-- <div class="service__slider-button"> 
+                                        <a href="services" class="">View More</a>
+                                    </div> -->
+                                </article>
+                            </div>
+                        </div>
+                        <div class="service__slider-wrap row pb-5">
+                            
+                            <div class="col-sm-6">
+                                <article class="home__service-block">
+                                    
+                                    <header>
+                                        <h2 class="service__header_1 section__sub-header">
+                                            Ceiling Interior Designing
+                                        </h2>
+                                    </header>
+
+                                    <div class="home__service-body">
+                                        <p>Experts in designing top-class ceilings depending on the type of space.</p>
+                                    </div>
+                                    <!-- <div class="service__slider-button"> 
+                                        <a href="services" class="">View More</a>
+                                    </div> -->
+                                </article>
+                            </div>
+                            <div class="col-sm-6">
+                                <figure >
+                                    <img src="img/services/commercial_interior_service_home.jpeg" alt="andsol commercial interior services">
+                                </figure>
+                            </div>
+                            
+                        </div>
+                        <div class="service__slider-wrap row pb-5">
+                            <div class="col-sm-6">
+                                    <figure >
+                                        <img src="img/services/residencial_interior_service_home.jpeg" alt="andsol residencial interior services">
+                                    </figure>
+                            </div>
+                            <div class="col-sm-6">
+                                <article class="home__service-block">
+                                    <header>
+                                        <h2 class="service__header_1 section__sub-header">
+                                            Interior Decorators
+                                        </h2>
+                                    </header>
+                                    <div class="home__service-body">
+                                        <p>From your room to your office, our expert designers are ready to design anything for you.</p>
+                                    </div>
+                                    <!-- <div class="service__slider-button"> 
+                                        <a href="services" class="">View More</a>
+                                    </div> -->
+                                </article>
+                            </div>
+                        </div>
+                        <div class="service__slider-wrap row pb-5">
+                            
+                            <div class="col-sm-6">
+                                <article class="home__service-block">
+                                    
+                                    <header>
+                                        <h2 class="service__header_1 section__sub-header">
+                                            Commercial
+                                        </h2>
+                                    </header>
+
+                                    <div class="home__service-body">
+                                        <p>Experts in designing commercial centers that demonstrates masterful utilization of space.</p>
+                                    </div>
+                                    <!-- <div class="service__slider-button"> 
+                                        <a href="services" class="">View More</a>
+                                    </div> -->
+                                </article>
+                            </div>
+                            <div class="col-sm-6">
+                                <figure >
+                                    <img src="img/services/commercial_interior_service_home.jpeg" alt="andsol commercial interior services">
+                                </figure>
+                            </div>
+                            
+                        </div>
+                        <div class="service__slider-wrap row pb-5">
+                            <div class="col-sm-6">
+                                    <figure >
+                                        <img src="img/services/residencial_interior_service_home.jpeg" alt="andsol residencial interior services">
+                                    </figure>
+                            </div>
+                            <div class="col-sm-6">
+                                <article class="home__service-block">
+                                    <header>
+                                        <h2 class="service__header_1 section__sub-header">
+                                            Interior Designers
+                                        </h2>
+                                    </header>
+                                    <div class="home__service-body">
+                                        <p>Creative interior designing services keeping the likes and dislikes of our customers in mind.</p>
+                                    </div>
+                                    <!-- <div class="service__slider-button"> 
+                                        <a href="services" class="">View More</a>
+                                    </div> -->
+                                </article>
+                            </div>
+                        </div>
+                        <div class="service__slider-wrap row pb-5">
+                            
+                            <div class="col-sm-6">
+                                <article class="home__service-block">
+                                    
+                                    <header>
+                                        <h2 class="service__header_1 section__sub-header">
+                                            PVC Interior Designers
+                                        </h2>
+                                    </header>
+
+                                    <div class="home__service-body">
+                                        <p> Giving you the house of your choice keeping your interior requirements in mind.</p>
+                                    </div>
+                                    <!-- <div class="service__slider-button"> 
+                                        <a href="services" class="">View More</a>
+                                    </div> -->
+                                </article>
+                            </div>
+                            <div class="col-sm-6">
+                                <figure >
+                                    <img src="img/services/commercial_interior_service_home.jpeg" alt="andsol commercial interior services">
+                                </figure>
+                            </div>
+                            
+                        </div>
+                        <div class="service__slider-wrap row pb-5">
+                            <div class="col-sm-6">
+                                    <figure >
+                                        <img src="img/services/residencial_interior_service_home.jpeg" alt="andsol residencial interior services">
+                                    </figure>
+                            </div>
+                            <div class="col-sm-6">
+                                <article class="home__service-block">
+                                    <header>
+                                        <h2 class="service__header_1 section__sub-header">
+                                            Residence
+                                        </h2>
+                                    </header>
+                                    <div class="home__service-body">
+                                        <p>We turn ideas into reality. With our team of experts we help you create your dream home.</p>
+                                    </div>
+                                    <!-- <div class="service__slider-button"> 
+                                        <a href="services" class="">View More</a>
+                                    </div> -->
+                                </article>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+    </div>
+<?php
+    include("../private/required/footer.php");
+?>
