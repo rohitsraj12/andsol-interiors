4238f5b1f1a7c0f24a8b840c99940b5743a60e83
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2021-02-21 13:05:18 +0530

    Create site_map.php

---
+++ public/site_map.php
@@ -0,0 +1,49 @@
+<?php
+ $page_title = "site map";
+ $page_meta_key = "";
+ $page_meta_description = "";
+
+    include("../private/required/header.php");
+?>
+
+    <div class="body__container space__py">
+        <main>
+
+            <section class="section__site-map top__space-p">
+                <div class="container">
+                    <div class="wrap__header py-4">
+                        <h2 class="section__header">
+                            site map
+                        </h2>
+                    </div>
+                    <!-- end wrap header -->
+                    <div class="site__map-content">
+                        <ul>
+                            <li>
+                                <a href="home">HOME</a>
+                            </li>
+                            <li>
+                                <a href="about">ABOUT US</a>
+                            </li>
+                            <li>
+                                <a href="services">SERVICES</a>
+                            </li>
+                            <li>
+                                <a href="gallery">GALLERY</a>
+                            </li>
+                            <li>
+                                <a href="contact">CONTACT US</a>
+                            </li>
+                            <li>
+                                <a href="faq">FAQs</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>
+
+<?php
+    include("../private/required/footer.php");
+?>
