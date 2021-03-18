a8c3fb1907cd1de6d438a005197c7eaa4d6b50b3
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2021-03-18 12:05:10 +0530

    Update commercial_services.php

--- public/commercial_services.php
+++ public/commercial_services.php
@@ -41,11 +41,11 @@
                                 <article class="home__service-block">
                                     <header>
                                         <h2 class="service__header_1 section__sub-header">
-                                            Interior Decorators
+                                            Office space
                                         </h2>
                                     </header>
                                     <div class="home__service-body">
-                                        <p>From your room to your office, our expert designers are ready to design anything for you.</p>
+                                        <p>From renovating the interiors of your office space to giving it a completely new look, we provide interior designs for office spaces that define class as well as make sure that you feel comfortable while working.</p>
                                     </div>
                                     <!-- <div class="service__slider-button"> 
                                         <a href="services" class="">View More</a>
@@ -60,12 +60,12 @@
                                     
                                     <header>
                                         <h2 class="service__header_1 section__sub-header">
-                                            Commercial
+                                        Restaurants/Pubs
                                         </h2>
                                     </header>
 
                                     <div class="home__service-body">
-                                        <p>Experts in designing commercial centers that demonstrates masterful utilization of space.</p>
+                                        <p>Be it a cozy restaurant with a soothing design or a restaurant that looks for colorful interiors, our experts will give you exactly what you are looking for. A designer coffee table or a wall with graffiti, we assure you to fulfill all your desires and give you the best.</p>
                                     </div>
                                     <!-- <div class="service__slider-button"> 
                                         <a href="services" class="">View More</a>
@@ -78,6 +78,28 @@
                                 </figure>
                             </div>
                             
+                        </div>
+                        <div class="service__slider-wrap row pb-5">
+                            <div class="col-sm-6">
+                                    <figure >
+                                        <img src="img/services/residencial_interior_service_home.jpeg" alt="andsol residencial interior services">
+                                    </figure>
+                            </div>
+                            <div class="col-sm-6">
+                                <article class="home__service-block">
+                                    <header>
+                                        <h2 class="service__header_1 section__sub-header">
+                                            PGs
+                                        </h2>
+                                    </header>
+                                    <div class="home__service-body">
+                                        <p>Comfortable PGs with the best of the interiors is what we offer. A home away from home that is designed to give you a homely feeling with all the comfort and necessities that will help you to live in it without any complaints.</p>
+                                    </div>
+                                    <!-- <div class="service__slider-button"> 
+                                        <a href="services" class="">View More</a>
+                                    </div> -->
+                                </article>
+                            </div>
                         </div>
                     </div>  
                 </section>
